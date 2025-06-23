ef617cf33d0bf9c9d47592624b012f661539b49a
Haykel Snoussi <dr.haykel.snoussi@gmail.com>
2025-06-23 14:14:19 -0400

    Update generate_tracto.sh

--- scripts/generate_tracto.sh
+++ scripts/generate_tracto.sh
@@ -1,28 +1,62 @@
 #/bin/bash
 
-for ID in 36; do
-for FILE in "wmfod" \
-              "sub-${ID}_mlp_fod" \
-              "sub-${ID}_fod_sCNN_model_march11_30percent_data"; do
-
-    FOD_PATH="/local/projects/scnn-investigation_haykel/protocols/sub-${ID}/${FILE}.nii.gz"
-    OUT_DIR="/local/projects/scnn-investigation_haykel/protocols/sub-${ID}/"
-    MASK_PATH="/local/projects/scnn-investigation_haykel/protocols/sub-${ID}/brain_mask.nii.gz"
-
-    tckgen "$FOD_PATH" \
-           "${OUT_DIR}/${FILE}_tracks01.tck" \
-           -algorithm iFOD2 \
-           -seed_dynamic "$FOD_PATH" \
-           -mask "$MASK_PATH" \
-           -select 100000 \
-           -cutoff 0.01 \
-           -step 0.5 \
-           -angle 20 \
-           -minlength 5 \
-           -maxlength 100 \
-           -force
+OUT_DIR="/local/projects/scnn-investigation_haykel/protocols/sub-36/"
+MASK_PATH="/local/projects/scnn-investigation_haykel/protocols/sub-36/brain_mask.nii.gz"
+FIVE_TT_MIF="/local/projects/scnn-investigation_haykel/protocols/sub-36/t2w/5tt_from_drawem2.nii.gz"
 
 
+# MSMT-CSD
+FOD_PATH="/local/projects/scnn-investigation_haykel/protocols/sub-36/wmfod2.nii.gz"
+tckgen "$FOD_PATH" \
+     "${OUT_DIR}/wmfod_tracks_june20_5tt.tck" \
+     -algorithm iFOD2 \
+     -seed_dynamic "$FOD_PATH" \
+     -mask "$MASK_PATH" \
+     -select 100000 \
+     -cutoff 0.001 \
+     -step 0.4 \
+     -angle 20 \
+     -minlength 5 \
+     -maxlength 300 \
+     -act "$FIVE_TT_MIF" \
+     -backtrack \
+     -crop_at_gmwmi \
+     -force
 
-  done
-done
+
+# sCNN
+FOD_PATH="/local/projects/scnn-investigation_haykel/protocols/sub-36/sub-36_fod_sCNN_model_june19_30percent_data.nii.gz"
+tckgen "$FOD_PATH" \
+     "${OUT_DIR}/scnn_tracks_june20_5tt.tck" \
+     -algorithm iFOD2 \
+     -seed_dynamic "$FOD_PATH" \
+     -mask "$MASK_PATH" \
+     -select 100000 \
+     -cutoff 0.001 \
+     -step 0.4 \
+     -angle 20 \
+     -minlength 5 \
+     -maxlength 300 \
+     -act "$FIVE_TT_MIF" \
+     -backtrack \
+     -crop_at_gmwmi \
+     -force
+
+
+# MLP
+FOD_PATH="/local/projects/scnn-investigation_haykel/protocols/sub-36/sub-36_mlp_june20_fod_abs.nii.gz"
+tckgen "$FOD_PATH" \
+     "${OUT_DIR}/mlp_tracks_june20_5tt.tck" \
+     -algorithm iFOD2 \
+     -seed_dynamic "$FOD_PATH" \
+     -mask "$MASK_PATH" \
+     -select 100000 \
+     -cutoff 0.001 \
+     -step 0.4 \
+     -angle 20 \
+     -minlength 5 \
+     -maxlength 300 \
+     -act "$FIVE_TT_MIF" \
+     -backtrack \
+     -crop_at_gmwmi \
+     -force
